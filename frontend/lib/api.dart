97e221a74450f008d9a30de6b8e1a04b13838187
Chart Chongcharoen <chartchuo@gmail.com>
2023-07-16 11:24:51 +0700

    change base url to localhost

--- frontend/lib/api.dart
+++ frontend/lib/api.dart
@@ -8,7 +8,8 @@ class Api {
   late Dio dio;
   var session = Session();
   var baseOptions = BaseOptions(
-    baseUrl: 'http://10.0.2.2:8000',
+    // baseUrl: 'http://10.0.2.2:8000',
+    baseUrl: 'http://localhost:8000',
     connectTimeout: const Duration(seconds: 5),
     receiveTimeout: const Duration(seconds: 5),
   );
