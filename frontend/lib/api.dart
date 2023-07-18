de2a6c9f9851719241640a3f7238d07d450ca5e5
Chart Chongcharoen <chartchuo@gmail.com>
2023-07-18 13:51:24 +0700

    fix macos

--- frontend/lib/api.dart
+++ frontend/lib/api.dart
@@ -9,7 +9,7 @@ class Api {
   var session = Session();
   var baseOptions = BaseOptions(
     // baseUrl: 'http://10.0.2.2:8000',
-    baseUrl: 'http://localhost:8000',
+    baseUrl: 'http://127.0.0.1:8000',
     connectTimeout: const Duration(seconds: 5),
     receiveTimeout: const Duration(seconds: 5),
   );
