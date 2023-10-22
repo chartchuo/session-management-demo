4103c62ea45e0ba3e13cf8e2c9f2e1f28f77ced1
Chart Chongcharoen <chartchuo@gmail.com>
2023-10-22 15:16:51 +0700

    add login test code

--- backend/login_test.go
+++ backend/login_test.go
@@ -11,7 +11,7 @@ func TestLogin(t *testing.T) {
 	router := setupRouter()
 
 	w := httptest.NewRecorder()
-	reader := strings.NewReader("{\"username\":\"admin\",\"password\":\"admin\"}")
+	reader := strings.NewReader("{\"username\":\"test\",\"password\":\"test\"}")
 
 	req, _ := http.NewRequest("POST", "/login", reader)
 	req.Header.Set("Content-Type", "application/json")
@@ -19,5 +19,27 @@ func TestLogin(t *testing.T) {
 	router.ServeHTTP(w, req)
 	if w.Code != 200 {
 		t.Errorf("error:%v", w.Body.String())
+	}
+
+	w = httptest.NewRecorder()
+	reader = strings.NewReader("{\"username\":\"admin\",\"password\":\"admin\"}")
+
+	req, _ = http.NewRequest("POST", "/login", reader)
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+	if w.Code != 200 {
+		t.Errorf("error:%v", w.Body.String())
+	}
+
+	w = httptest.NewRecorder()
+	reader = strings.NewReader("{\"username\":\"user\",\"password\":\"incorrect password\"}")
+
+	req, _ = http.NewRequest("POST", "/login", reader)
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+	if w.Code == 200 {
+		t.Errorf("error:%v", "expect error but sucess")
 	}
 }
